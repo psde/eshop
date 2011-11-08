51195dd45fa8215a2e58ff7ffbea64e6d2ccc8a3
Boris Filipov <bfilipov@gmail.com>
2011-11-08 07:58:30 +0100

    Refactored the admin field according to the convention.

--- vis/WebContent/WEB-INF/src/eshop/pojo/User.java
+++ vis/WebContent/WEB-INF/src/eshop/pojo/User.java
@@ -9,7 +9,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = -8276035923596243251L;
 	private String username;
 	private String password;
-	private boolean is_admin;
+	private boolean admin;
 	
 	public String getUsername() {
 		return username;
@@ -23,10 +23,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public boolean isIs_admin() {
-		return is_admin;
+	public boolean isAdmin() {
+		return admin;
 	}
-	public void setIs_admin(boolean is_admin) {
-		this.is_admin = is_admin;
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
 	}
 }
