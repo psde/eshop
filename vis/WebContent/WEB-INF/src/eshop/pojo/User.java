bb19edd6784d277b8b38992e180e5a5954ec74fb
Boris Filipov <bfilipov@gmail.com>
2011-11-08 23:27:52 +0800

    The hibernate Mapping for the User class is now defined using Annotations.

--- vis/WebContent/WEB-INF/src/eshop/pojo/User.java
+++ vis/WebContent/WEB-INF/src/eshop/pojo/User.java
@@ -2,11 +2,18 @@ package eshop.pojo;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String username;
 	private String password;
