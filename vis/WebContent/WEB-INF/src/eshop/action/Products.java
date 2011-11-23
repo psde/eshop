2fb54fc905a897ede4967eaa3077036abe7dfd56
borfd <bfilipov@gmail.com>
2011-11-23 17:50:58 +0800

    Products field.

--- vis/WebContent/WEB-INF/src/eshop/action/Products.java
+++ vis/WebContent/WEB-INF/src/eshop/action/Products.java
@@ -1,10 +1,14 @@
 package eshop.action;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.Session;
 
 import com.opensymphony.xwork2.ActionSupport;
+
+import eshop.pojo.Product;
 
 public class Products extends ActionSupport implements SessionAware {
 
@@ -13,14 +17,27 @@ public class Products extends ActionSupport implements SessionAware {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Map session;
+	private List<Product> products;
 	
 	@Override
 	public void setSession(Map<String, Object> arg0) {
 		session = arg0;	
 	}
 	
+	public Map getSession() {
+		return session;
+	}
+	
 	public String execute() throws Exception {
 		return SUCCESS;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
 	}
 
 }
